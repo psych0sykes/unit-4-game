f3ec9b15178ab33c3c1cfa6bc769b3ff1f4170db
Blake Sykes <blakesykes13@gmail.com>
2019-09-12 01:15:12 -0600

    Added functions to dynamically generate chars and to move them to the battle area on click. Linked attack function to button. add some css to make it look a bit better.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,6 +1,6 @@
 window.onload = function () {
 
-console.log("JS READY")
+console.log("JS READY");
 
 
 var char = [
@@ -13,82 +13,122 @@ var char = [
     char3 = {
     }
 ];
-var user;
-var enemy;
+var user = {
+    "isUser" : false,
+};
+var enemy = {
+    "isEnemy" : false,
+};
 var increase;
+var defeated;
 
 function setChar () {
     char = [
         char0 = {
-            "name" : "char0",
+            "name" : "croissant",
             "hp" : 100,
             "attack" : 3,
             "counter" : 5,
             "isUser" : false,
-            "isSelected" : false,
+            "isEnemy" : false,
             "isDead" : false,
+            "img" : "assets/images/croissant.png"
         },
         char1 = {
-            "name" : "char1",
+            "name" : "cupcake",
             "hp" : 90,
             "attack" : 5,
             "counter" : 7,
             "isUser" : false,
-            "isSelected" : false,
+            "isEnemy" : false,
             "isDead" : false,
+            "img" : "assets/images/cupcake.png"
         },
         char2 = {
-            "name" : "char2",
+            "name" : "pie",
             "hp" : 80,
             "attack" : 7,
             "counter" : 9,
             "isUser" : false,
-            "isSelected" : false,
+            "isEnemy" : false,
             "isDead" : false,
+            "img" : "assets/images/pie.png"
         },
         char3 = {
-            "name" : "char3",
+            "name" : "cookie",
             "hp" : 70,
             "attack" : 9,
             "counter" : 11,
             "isUser" : false,
-            "isSelected" : false,
+            "isEnemy" : false,
             "isDead" : false,
+            "img" : "assets/images/cookie.png"
         }
     ];
-}
+    user = null;
+    enemy = null;
+    user = {
+        "isUser" : false,
+    };
+    enemy = {
+        "isEnemy" : false,
+    };
+};
 
-console.log(char[0]["name"])
+function start () {
+    setChar();
 
-setChar()
 
-console.log(char[0]["name"])
+    for (var i = 0; i < char.length; i++) {
+        var newDiv = $("<div>");
+        newDiv.addClass("icon");
+        newDiv.html("<img src ='" + char[i]["img"] + "'>");
+        newDiv.attr("id",char[i]["name"])
+        newDiv.attr("charNumber",[i])
+        $("#charSelect").append(newDiv);
+    }
+    
+};
+
+console.log(char[0]["name"]);
+
+setChar();
+
+console.log(char[0]["name"]);
 
 function selectEnemy (a) {
-    char[a]["isSelected"] = true;
-    enemy = char[a]
-    console.log(enemy)
+    char[a]["isEnemy"] = true;
+    enemy = char[a];
+    console.log(enemy);
 }
 function selectUser (a) {
     char[a]["isUser"] = true;
-    user = char[a]
-    increase = user["attack"]
-    console.log(user)
+    user = char[a];
+    increase = user["attack"];
+    console.log(user);
 }
 
-selectUser(0)
-selectEnemy(1)
+function ifDead () {
+    if (enemy["hp"] < 1) {
+        enemy = null;
+        enemy = {"isEnemy" : false}
+        defeated++;
+        $("#enemyDiv").empty();
+        console.log("ifDead");
+    }
+    if (user["hp"] < 1) {
+        alert("LOSER");
+        start();
+    }
+    win();
+}
 
-// function ifDead () {
-//     for (var i = 0; i < char.length; i++) {
-//         if (char[i]["isDead"]) {
-//             console.log(char[i]["name"] + " is DEAD")
-//         }
-//         else {
-//             console.log(char[i]["name"] + " is ALIVE")
-//         }
-//     }
-// }
+function win () {
+    if (defeated === 3) {
+        alert("WINNER")
+        start()
+    }
+}
 
 function attack () {
     enemy["hp"] = enemy["hp"] - user["attack"];
@@ -96,9 +136,27 @@ function attack () {
     user["hp"] = user["hp"] - enemy["counter"];
     console.log(user);
     console.log(enemy);
+    ifDead();
 };
 
-attack();
+start()
+
+$(".icon").click(function () {
+
+    if (user["isUser"] === false) {
+        var a = $(this).attr("charNumber")
+        selectUser(a)
+        $("#userDiv").append(this);
+    }
+    else if (enemy["isEnemy"] === false) {
+        var a = $(this).attr("charNumber")
+        selectEnemy(a)
+        $("#enemyDiv").append(this);
+    }
+});
+
+$("#attackButton").click(attack)
+
 
 
 };
